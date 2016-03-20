19f709583e19a10ec533e8d745890952a1ced5c1
niproblema <jan.aleksandrov@gmail.com>
2016-03-20 09:50:10 +0000

    Dinamika gumba za ustavitev stroboskopa

--- skripta.js
+++ skripta.js
@@ -1,49 +1,57 @@
 window.addEventListener('load', function() {
 	//stran nalozena
-	
+
 	//Dodaj novo barvo
 	var dodajBarvo = function(event) {
 		var input = document.createElement('button');
-        var picker = new jscolor(input);
-        picker.fromRGB(Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255))
-        document.getElementById("barve").appendChild(input);
+		var picker = new jscolor(input);
+		picker.fromRGB(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255))
+		document.getElementById("barve").appendChild(input);
 	}
-	
-	document.querySelector("#novaBarva") 
+
+	document.querySelector("#novaBarva")
 		.addEventListener('click', dodajBarvo);
-		
+
 	//Odstrani barve
-	var odstraniBarve = function(event){
+	var odstraniBarve = function(event) {
 		var grupaBarv = document.getElementById("barve")
-		while(grupaBarv.firstChild){
+		while (grupaBarv.firstChild) {
 			grupaBarv.removeChild(grupaBarv.lastChild);
 		}
 	}
-	document.querySelector("#odstraniBarve") 
-	.addEventListener('click', odstraniBarve);
-	
+	document.querySelector("#odstraniBarve")
+		.addEventListener('click', odstraniBarve);
+
 	//Stroboskop
 	var vrednosti = [];
 	var minCas = 0;
 	var maxCas = 0;
 	var ustavi = false;
-	
+
 	var spremeniBarvo = function(id) {
-		document.getElementById("stroboskop").style.backgroundColor = "#"+vrednosti[id];
+		document.getElementById("stroboskop").style.backgroundColor = "#" + vrednosti[id];
 
 		if (ustavi) {
 			ustavi = false;
-		} else {
-			novId = (id+1) % vrednosti.length;
-			timeout = Math.floor((Math.random() * (maxCas-minCas)) + minCas);
-			setTimeout(function() {spremeniBarvo(novId)} , timeout);
-		}		
+
+		}
+		else {
+			novId = (id + 1) % vrednosti.length;
+			timeout = Math.floor((Math.random() * (maxCas - minCas)) + minCas);
+			setTimeout(function() {
+				spremeniBarvo(novId)
+			}, timeout);
+		}
 	}
-	
+
 	var stop = function(event) {
 		ustavi = true;
+		var start = document.querySelector("#start");
+		start.innerHTML = "Zaženi stroboskop";
+		start.removeEventListener('click', stop);
+		start.addEventListener('click', zagon);
 	}
-	
+
 	var zagon = function(event) {
 		vrednosti = [];
 		var barve = document.querySelectorAll("#barve > button");
@@ -51,17 +59,17 @@ window.addEventListener('load', function() {
 			var barva = barve[i];
 			vrednosti.push(barva.innerHTML);
 		}
-		
+
 		minCas = 1000;
 		maxCas = 1000;
 		spremeniBarvo(0);
-		
+
 		var start = document.querySelector("#start");
 		start.innerHTML = "Ustavi stroboskop";
 		start.removeEventListener('click', zagon);
 		start.addEventListener('click', stop);
 	}
-	
+
 	document.querySelector("#start").addEventListener('click', zagon);
-	
+
 });
